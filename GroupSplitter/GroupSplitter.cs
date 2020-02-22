bab387ed3cd1bd8257fd19e94f2a9ed1eb3d5a73
Michał Stadryniak <stadryniak.michal@gmail.com>
2020-02-22 21:51:03 +0100

    group splitter part 1

--- GroupSplitter/GroupSplitter.cs
+++ GroupSplitter/GroupSplitter.cs
@@ -6,5 +6,30 @@ namespace GroupSplitter
 {
     class GroupSplitter
     {
+        public List<List<Member>> GroupsMemList { get; }
+        private int _groupCount;
+        public int GroupCount
+        {
+            get => _groupCount;
+            set
+            {
+                _groupCount = value;
+                if (GroupsMemList == null) return;
+                List<Member> tmpList = new List<Member>();
+                foreach (var list in GroupsMemList)
+                {
+                    tmpList.AddRange(list);
+                }
+                SplitIntoGroups(tmpList);
+            }
+        }
+
+        public void SplitIntoGroups(List<Member> members)
+        {
+            foreach (var member in members)
+            {
+
+            }
+        }
     }
 }
